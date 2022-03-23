5c0b51bf2ea191efe6164d65ab156ba72e185d5d
Matt Hanes <hanes.matt@gmail.com>
2022-03-23 15:53:32 -0400

    Update TagsList.js
    Lighten the hover text color.

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -36,7 +36,7 @@ const TagsList = () => {
           const [text, value] = tag
           const tagSlug = slugify(text, { lower: true })
           return (
-            <Link className="capitalize block transition-all ease-in-out duration-300 hover:text-indigo-700" to={`/tags/${tagSlug}`} key={index}>
+            <Link className="capitalize block transition-all ease-in-out duration-300 hover:text-indigo-500" to={`/tags/${tagSlug}`} key={index}>
               {text} ({value})
             </Link>
           )

1158de0fb2a027f7eebc9591478d5b15a9396c73
Matt Hanes <hanes.matt@gmail.com>
2023-11-29 11:18:17 -0500

    Try TouchEnd

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React, {useRef} from 'react'
 const SearchBar = ({ handleChange, searchTerm }) => {
   const inputRef = useRef(null)
 
-  const handleFocus = () => {
+  const handleTouchEnd = () => {
     inputRef.current && inputRef.current.scrollIntoView({ behavior: 'smooth' })
   }
 
@@ -13,7 +13,7 @@ const SearchBar = ({ handleChange, searchTerm }) => {
       placeholder="Search Recipes"
       value={searchTerm}
       onChange={handleChange}
-      onFocus={handleFocus}
+      onTouchEnd={handleTouchEnd}
       ref={inputRef}
       className="mb-4 mt-2 w-full rounded-lg border border-indigo-200 bg-white px-4 py-2 text-gray-700 placeholder-gray-600 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 dark:bg-zinc-800 dark:text-gray-300 dark:placeholder-gray-400 lg:mb-8"
     />

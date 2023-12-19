8cc7e69b7b22d301b3b68f25b16e21251298a47b
Matt Hanes <hanes.matt@gmail.com>
2023-12-18 19:07:15 -0500

    Better handling of fractions

--- src/utils/decimalToFraction.js
+++ src/utils/decimalToFraction.js
@@ -1,21 +1,33 @@
-const getFraction = (decimal) => {
-  const tolerance = 1.0E-2
-  let numerator = 1
-  let denominator = 1
-  let error = decimal - numerator / denominator
-  while (Math.abs(error) > tolerance) {
-    if (error < 0) {
-      denominator++
-    } else {
-      numerator++
+const getFraction = decimal => {
+  console.log(decimal)
+  const fractions = [
+    { numerator: 1, denominator: 2 },
+    { numerator: 1, denominator: 4 },
+    { numerator: 1, denominator: 3 },
+    { numerator: 3, denominator: 4 },
+    { numerator: 2, denominator: 3 },
+  ]
+
+  let closestFraction = fractions[0]
+  let smallestDifference = Math.abs(
+    decimal - closestFraction.numerator / closestFraction.denominator
+  )
+
+  for (let i = 1; i < fractions.length; i++) {
+    const difference = Math.abs(
+      decimal - fractions[i].numerator / fractions[i].denominator
+    )
+    if (difference < smallestDifference) {
+      smallestDifference = difference
+      closestFraction = fractions[i]
     }
-    error = decimal - numerator / denominator
   }
-  const result = `${numerator}/${denominator}`
+
+  const result = `${closestFraction.numerator}/${closestFraction.denominator}`
   return result
 }
 
-const decimalToFraction = (amount) => {
+const decimalToFraction = amount => {
   const float = parseFloat(amount)
   if (isNaN(float)) {
     return amount
@@ -25,9 +37,12 @@ const decimalToFraction = (amount) => {
   if (decimal === 0) {
     return wholeNumber
   }
+  if (decimal > 0.9) {
+    return wholeNumber + 1
+  }
   const fraction = getFraction(decimal)
   const result = `${wholeNumber !== 0 ? wholeNumber + ' ' : ''}${fraction}`
   return result
 }
 
-export default decimalToFraction;
+export default decimalToFraction

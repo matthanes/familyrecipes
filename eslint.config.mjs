5960c4e2d54e909b0c95141a4666748c154ee64b
Matt Hanes <hanes.matt@gmail.com>
2025-11-28 19:39:48 -0500

    Remove legacy partytown

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import nextConfig from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = [
   {
-    ignores: [".next/**", "node_modules/**", "public/~partytown/**", ".cache/**"],
+    ignores: [".next/**", "node_modules/**", ".cache/**"],
   },
   ...nextConfig,
 ];
